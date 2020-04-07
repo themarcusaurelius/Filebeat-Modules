9f2afa831a93eeb7757e01a763e6c00b2f49830b
Mark Mayfield <mmayfield@panzura.com>
2020-04-07 11:58:01 -0500

    Changed Filebeat path

--- installFilebeat.ps1
+++ installFilebeat.ps1
@@ -8,7 +8,7 @@ if($principal.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) 
 
     "`nYou are running Powershell with full privilege`n"
 
-    Set-Location -Path 'c:\filebeat-master\filebeat'
+    Set-Location -Path 'c:\filebeat-modules-master\filebeat'
     Set-ExecutionPolicy Unrestricted
     
     "Filebeatbeat Execution policy set - Success`n"
